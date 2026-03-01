bc415b08ba6882705380a1e96f4b685c2ebed686
Marco Dilaudo <Marco_dilaudo1@baylor.edu>
2026-02-28 23:08:44 -0600

    Updated results for OOP assignment

--- FoodClass.py
+++ FoodClass.py
@@ -1,76 +1,48 @@
 class Customer:
-    def __init__(self, customer_id: int, name: str, address: str, email: str, phone: str, member_status: bool):
-        self.__customer_id = customer_id
-        self.__name = name
-        self.__address = address
-        self.__email = email
-        self.__phone = phone
-        self.__member_status = member_status
+    def __init__(self, customer_id, name, address, email, phone, member_status):
+        self._customer_id = customer_id
+        self._name = name
+        self._address = address
+        self._email = email
+        self._phone = phone
+        self._member_status = member_status
 
-        self.__items_ordered = []
-        self.__subtotal = 0.0
-        self.__discount_amount = 0.0
+    #Accessor Methods
 
-    # Accessors
     def get_customer_id(self):
-        return self.__customer_id
+        return self._customer_id
 
     def get_name(self):
-        return self.__name
+        return self._name
 
     def get_address(self):
-        return self.__address
+        return self._address
 
     def get_email(self):
-        return self.__email
+        return self._email
 
     def get_phone(self):
-        return self.__phone
+        return self._phone
 
     def get_member_status(self):
-        return self.__member_status
-
-    # Mutating
-    def charge_customer(self, transaction):
-        price = float(transaction.get_cost())
-        self.__items_ordered.append(transaction)
-        self.__subtotal += price
-
-        # 20% discount if member
-        if self.__member_status:
-            self.__discount_amount += price * 0.20
-
-    def report(self):
-        print(f"Customer Name: {self.__name}")
-        print(f"Phone: {self.__phone}")
-
-        for trans in self.__items_ordered:
-            print(f"Order Item: {trans.get_item_name()} Price: ${trans.get_cost():,.2f}")
-
-        print(f"Total Cost: ${self.__subtotal:,.2f}")
-
-        if self.__member_status:
-            print(f"Member Discount: ${self.__discount_amount:,.2f}")
-            total_after = self.__subtotal - self.__discount_amount
-            print(f"Total Cost after discount: ${total_after:,.2f}")
+        return self._member_status
 
 
 class Transaction:
-    def __init__(self, date: str, item_name: str, cost: float, customer_id: int):
-        self.__date = date
-        self.__item_name = item_name
-        self.__cost = float(cost)
-        self.__customer_id = int(customer_id)
+    def __init__(self, date, item_name, cost, customer_id):
+        self._date = date
+        self._item_name = item_name
+        self._cost = cost
+        self._customerid = customer_id
 
-    # Accessors
     def get_date(self):
-        return self.__date
+        return self._date
 
     def get_item_name(self):
-        return self.__item_name
+        return self._item_name
 
     def get_cost(self):
-        return self.__cost
+        return self._cost
 
-    def get_customer_id(self):
-        return self.__customer_id
+    def get_customerid(self):
+        return self._customer_id
--- FoodProgram.py
+++ FoodProgram.py
@@ -1,39 +1,40 @@
 import FoodClass as fc
 
-# this dictionary represents transactions. The key of the dictionary is the transaction identifier.
-# The Value of the dictionary is a list. The elements in each list are -
-# ['Date', 'Name of item', 'Cost', 'customerid' ]
-
+# This dictionary represents transactions
+# The key is the transaction identifier
+# Each value is a list: ['Date', 'Name of item', 'Cost', 'customerid']
 dict = {'trans1':['2/15/2023','The Lone Patty',17,569],
                 'trans2':['2/15/2023','The Octobreakfast',18,569],
                 'trans3':['2/15/2023','The Octoveg',16,570],
                 'trans4':['2/15/2023','The Octoburger',20,570],
 }
 
-DISCOUNT_RATE = 0.2
+#Customer class
+customer_one = fc.Customer(569, 'Aubree Himsworth', '1172 Moulton Hill Waco Texas 76710', 'ahimsworthfs@list-manage.com', '254-555-2273', True)
+customer_two = fc.Customer(570, 'Danni Sellyar', '97 Mitchell Way Hewitt Texas 76712', 'dsellyarft@gmpg.org', '254-555-9362', False)
 
-def main():
-    order_total = 0
+#Built a switch for the customer we want to run
+active_customer = customer_one
+#active_customer = customer_two
+#Can comment out either line above for the switch
 
-customers = {
-    569: ["Aubree Himsworth", "1172 Moulton Hill Waco Texas 76710", "ahimsworthfs@list-manage.com", "254-555-2273", True],
-    570: ["Danni Sellyar", "97 Mitchell Way Hewitt Texas 76712", "dsellyarft@gmpg.org", "254-555-9362", False]
-}
+#Customer Header
+print(f"Customer Name: {active_customer.get_name()}")
+print(f"Phone: {active_customer.get_phone()}")
 
-def main():
-    # Pull customer id
-    input_id = int(input("Enter customer ID: "))
+#Transaction object
+order_total = 0
+for entry in dict.values():
+    transaction = fc.Transaction(entry[0], entry[1], entry[2], entry[3])
+    
+    if transaction.get_customerid() == active_customer.get_customer_id():
+        print(f"Order Item: {transaction.get_item_name()}  Price: ${transaction.get_cost():.2f}")
+        order_total += transaction.get_cost()
 
-    # Customer instance
-    customer_data = customers[input_id]
-    customer = fc.Customer(input_id, *customer_data)
+#Total + Discount if member
+print(f"Total Cost: ${order_total:.2f}")
 
-    # Transaction objects
-    for data in dict.values():
-        trans = fc.Transaction(*data)
-        if trans.get_customer_id() == customer.get_customer_id():
-            customer.charge_customer(trans)
-
-    customer.report()
-
-main()
+if active_customer.get_member_status() == True:
+    discount = order_total * 0.20
+    print(f"Member Discount: ${discount:.2f}")
+    print(f"Total Cost after discount: ${order_total - discount:.2f}")
